78f5456bfe45616f051114579eba4af5f23aed44
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-08-22 12:50:54 -0300

    feat(api): add sync Googlecalendar with database

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const { oauth2Client, authUrl } = require('./config/oauth2');
 const eventRoutes = require('./routes/eventRoutes');
+const { handleOAuth2Callback } = require('./controllers/authController');
 
 const app = express();
 const port = 3000;
@@ -17,29 +18,15 @@ app.use(express.static(path.join(__dirname, 'views')));
 
 // Rota principal
 app.get('/', (req, res) => {
-    res.send(`<a href="${authUrl}">Autenticar com o Google Calendar</a>`);
+    res.send(`
+        <h1>Bem-vindo!</h1>
+        <p>Faça login com o Google para sincronizar seu Google Calendar com o banco de dados.</p>
+        <a href="${authUrl}">Login com o Google</a>
+    `);
 });
 
 // Rota de callback do OAuth2
-app.get('/oauth2callback', async (req, res) => {
-    const { code } = req.query;
-    if (!code) {
-        return res.status(400).send('Código de autorização ausente.');
-    }
-
-    try {
-        const { tokens } = await oauth2Client.getToken(code);
-        oauth2Client.setCredentials(tokens);
-        console.log('Tokens:', tokens);
-        res.send(`
-            <p>Autenticação concluída! Agora você pode criar eventos.</p>
-            <a href="/events/create-event-form">Clique aqui para criar um evento</a>
-        `);
-    } catch (error) {
-        console.error('Erro ao obter o token de autenticação:', error);
-        res.status(500).send('Erro ao concluir a autenticação.');
-    }
-});
+app.get('/oauth2callback', handleOAuth2Callback);
 
 // Rotas para eventos
 app.use('/events', eventRoutes);
