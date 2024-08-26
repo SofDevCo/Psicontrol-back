d2f2f63d743dbe62ae409e0752deb5d048e7b88b
Fabiano-fnc <fabiano_fnc2003@hotmail.com>
2024-08-26 11:46:52 -0300

    feat(api): added function to delete by calendar

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const { google } = require('googleapis');
-const { createEvent: saveEvent, eventExists } = require('../services/eventService');
+const { createEvent: saveEvent, eventExists, deleteEventByGoogleId } = require('../services/eventService');
 const { oauth2Client, authUrl } = require('../config/oauth2');
+const { Evento } = require('../models/eventModel');
 
 const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
 
@@ -23,18 +24,17 @@ const syncGoogleCalendarWithDatabase = async (accessToken) => {
             if (!existingEvent) {
                 let startDate = null, startTime = null, endTime = null;
 
-                // Verifica se o evento tem `dateTime` ou `date`
                 if (event.start && event.start.dateTime) {
                     startDate = event.start.dateTime.split('T')[0];
                     startTime = event.start.dateTime.split('T')[1].split(':').slice(0, 2).join(':');
                     endTime = event.end.dateTime.split('T')[1].split(':').slice(0, 2).join(':');
                 } else if (event.start && event.start.date) {
                     startDate = event.start.date;
-                    startTime = "00:00"; // Opcional: Definir horário de início como meia-noite
-                    endTime = "23:59";   // Opcional: Definir horário de término como 23:59
+                    startTime = "00:00";
+                    endTime = "23:59";
                 }
 
-                const newEvent = await saveEvent({
+                await saveEvent({
                     event_name: event.summary || 'Sem título',
                     date: startDate,
                     start_time: startTime,
@@ -43,9 +43,28 @@ const syncGoogleCalendarWithDatabase = async (accessToken) => {
                 });
             }
         }
+
+        await deleteNonexistentGoogleEvents(events);
+
     } catch (error) {
         console.error('Erro ao sincronizar eventos com o banco de dados:', error);
         throw new Error('Erro ao sincronizar eventos.');
+    }
+};
+
+const deleteNonexistentGoogleEvents = async (events) => {
+    try {
+        const googleEventIds = events.map(event => event.id);
+        const allDbEvents = await Evento.findAll();
+
+        for (const dbEvent of allDbEvents) {
+            if (!googleEventIds.includes(dbEvent.google_event_id)) {
+                await deleteEventByGoogleId(dbEvent.google_event_id);
+            }
+        }
+    } catch (error) {
+        console.error('Erro ao deletar eventos inexistentes no Google Calendar:', error);
+        throw new Error('Erro ao deletar eventos inexistentes no Google Calendar.');
     }
 };
 
@@ -64,7 +83,7 @@ async function handleOAuth2Callback(req, res) {
         oauth2Client.setCredentials(tokens);
         await syncGoogleCalendarWithDatabase(tokens.access_token);
         res.redirect('/events/create-event-form');
-    } catch (error) {   
+    } catch (error) {
         console.error('Erro ao obter o token de autenticação:', error);
         res.status(500).send('Erro ao concluir a autenticação.');
     }
