e75369b83a1b5e6c4ae14c0bda225b102fbb8bc7
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-09-30 14:21:37 -0300

    fix(api):"remove routes"

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const { oauth2Client, authUrl } = require('./config/oauth2');
 const eventRoutes = require('./routes/eventRoutes');
 const revenueRoutes = require('./routes/revenueRoutes'); // Importa as rotas de receitas e despesas
-const { handleOAuth2Callback, initiateGoogleAuth, getUserId } = require('./controllers/authController');
+const { handleOAuth2Callback, initiateGoogleAuth} = require('./controllers/authController');
 const { verifyToken } = require('./middleware/authMiddleware');
 const app = express();
 const port = 3000;
@@ -23,23 +23,18 @@ app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('API de Eventos. Use as rotas disponíveis para gerenciar eventos.');
 });
 
-app.get('/google', initiateGoogleAuth);
-app.get('/oauth2callback', handleOAuth2Callback);
-app.use('/events',eventRoutes);
-app.use('/income',revenueRoutes); 
+app.get('/google',initiateGoogleAuth);
+app.get('/oauth2callback',handleOAuth2Callback);
+app.use('/events', verifyToken, eventRoutes);
+app.use('/income', verifyToken, revenueRoutes); 
 app.get('/auth/google/callback', handleOAuth2Callback);
-app.get('/get-user-id', getUserId);
-
-    // if (req.session && req.session.userId) {
-    //     res.json({ userId: req.session.userId });
-    // } else { res.status(401).send('User not found')};
-
-
 require('./cronjob/cronJob');
 
 app.listen(port, () => {
