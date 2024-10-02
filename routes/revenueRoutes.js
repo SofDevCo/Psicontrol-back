83c410cba74dadcecc8b8eff7122a65c0f4b1c3b
Fabiano-fnc <fabiano_fnc2003@hotmail.com>
2024-10-02 11:27:01 -0300

    feat(fix): We fixed the income and expenses page

--- routes/revenueRoutes.js
+++ routes/revenueRoutes.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const revenueController = require('../controllers/revenueController'); // Importa o controller
+const revenueController = require('../controllers/revenueController'); 
 
-router.post('/revenue/:user_id', revenueController.Revenue);
+router.post('/revenue/:user_id', revenueController.addRevenue);
 router.post('/expense/:user_id', revenueController.addExpense);
+
+router.get('/revenue/:user_id', revenueController.getRevenueByUserId);
+router.get('/expense/:user_id', revenueController.getExpenseByUserId);
 
 router.get('/entries/:user_id', revenueController.getEntriesByUserId);
 
 router.delete('/revenue/:id', revenueController.deleteRevenue);
 router.delete('/expense/:id', revenueController.deleteExpense);
-
 
 
 module.exports = router;
