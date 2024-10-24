17c18b9eb4b8fb018991a2268c64daceb1459d55
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-10-24 13:45:06 -0300

    feat(api):"create function for calculate age"

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -51,3 +51,17 @@ exports.formatDateIso = (date) => {
 
   return format(date, "yyyy-MM-dd", { locale: ptBR });
 };
+
+exports.calculateAge = (dateOfBirth) => {
+  const dob = new Date(dateOfBirth);
+  const today = new Date(); 
+  let age = today.getFullYear() - dob.getFullYear();
+
+
+  const monthDifference = today.getMonth() - dob.getMonth();
+  if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < dob.getDate())) {
+    age--; 
+  }
+
+  return age;
+}
