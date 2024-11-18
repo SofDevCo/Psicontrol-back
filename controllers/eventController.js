9bff63b79a1ff32af14beb084f22e2f10f2107de
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-11-18 17:11:35 -0300

    fix(api)"remove consultationDays in EventController"

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -216,19 +216,3 @@ exports.saveSelectedCalendars = async (req, res) => {
 };
 
 
-exports.updateConsultationDays = async (customerId) => {
-  const events = await Event.findAll({
-    where: { customer_id: customerId },
-    attributes: ["date"], 
-  });
-
-  const days = [...new Set(events.map(event => new Date(event.date).getDate()))]
-    .sort((a, b) => a - b)
-    .join(", ");
-
-  await CustomersBillingRecords.update(
-    { consultation_days: days },
-    { where: { customer_id: customerId } }
-  );
-};
-
