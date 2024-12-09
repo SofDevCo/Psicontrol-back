027cd83036e3256197a1126a18de4c15df41705d
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-12-09 14:32:10 -0300

    fix(api):"delete for change name files"

--- routes/whatsappRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const whatsappController = require("../controllers/whatsappController");
-
-router.post('/send-whatsapp',whatsappController.sendWhatsAppMessage);
-
-module.exports = router;
