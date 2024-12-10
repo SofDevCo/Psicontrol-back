7d6776d8ff22530bb9ed6cbc8541e4233e0c5d7d
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-12-09 21:30:51 -0300

    feat(api):"DATABASE_URL"

--- config/database.js
+++ config/database.js
@@ -2,6 +2,8 @@ const { Sequelize } = require('sequelize');
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
 
-const sequelize = new Sequelize(DATABASE_URL)
+const sequelize = new Sequelize(DATABASE_URL,{
+    dialect: 'postgres',
+})
 
 module.exports = { sequelize }; 
