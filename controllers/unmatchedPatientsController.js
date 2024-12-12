664c643d42ae800b157fef9a2fe8280903e0a846
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-12-12 10:51:24 -0300

    feat(api):"add google_event_id"

--- controllers/unmatchedPatientsController.js
+++ controllers/unmatchedPatientsController.js
@@ -1,5 +1,4 @@
-const { Customer } = require("../models");
-const { Event } = require("../models");
+const { Customer,Event } = require("../models");
 const { syncGoogleCalendarWithDatabase } = require("./authController");
 
 
@@ -9,10 +8,11 @@ exports.getUnmatchedPatients = async (req, res) => {
       user_id: req.user.user_id,
       customer_id: null,
     },
-    attributes: ['customers_id', 'event_name', 'date'],
+    attributes: ['customers_id', 'event_name', 'date', 'google_event_id'],
   });
 
   const response = unmatchedEvents.map(event => ({
+    google_event_id: event.google_event_id,
     customers_id: event.customers_id,
     name: event.event_name,
     date: event.date,
@@ -20,3 +20,4 @@ exports.getUnmatchedPatients = async (req, res) => {
 
   res.json(response);
 };
+
