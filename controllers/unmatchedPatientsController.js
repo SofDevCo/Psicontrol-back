90a80e592c0dff28b2998d2563a2775b73bc1aac
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-12-13 12:11:35 -0300

    feat(api):"get events calendars enabled"

--- controllers/unmatchedPatientsController.js
+++ controllers/unmatchedPatientsController.js
@@ -1,14 +1,31 @@
-const { Customer,Event } = require("../models");
-const { syncGoogleCalendarWithDatabase } = require("./authController");
+const { Customer,Event, Calendar } = require("../models");
 
+exports.getUnmatchedPatients = async (req, res, next) => {
 
-exports.getUnmatchedPatients = async (req, res) => {
+  const enabledCalendars = await Calendar.findAll({
+    where: {
+      user_id: req.user.user_id,
+      enabled: true,
+    },
+    attributes: ['calendar_id'],
+  })
+
+  const calendarIds = enabledCalendars.map((calendar) => calendar.calendar_id );
+
+  if(calendarIds.length === 0 ){
+    return res.status(200).json({
+      message: "Nenhum calendário sincronizado",
+      unmatchedEvents: [],
+    })
+  }
+
   const unmatchedEvents = await Event.findAll({
     where: {
       user_id: req.user.user_id,
       customer_id: null,
+      calendar_id: calendarIds,
     },
-    attributes: ['customers_id', 'event_name', 'date', 'google_event_id'],
+    attributes: ['customers_id', 'event_name', 'date', 'google_event_id', 'calendar_id'],
   });
 
   const response = unmatchedEvents.map(event => ({
@@ -16,6 +33,7 @@ exports.getUnmatchedPatients = async (req, res) => {
     customers_id: event.customers_id,
     name: event.event_name,
     date: event.date,
+    calendar_id: event.calendar_id,
   }));
 
   res.json(response);
