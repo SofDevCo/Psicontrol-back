377d68bcc16e4eea4fb9d31819a8819c1c2862ea
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2024-12-19 11:51:44 -0300

    feat(api): "partial payment functionality to billing records"

--- controllers/dashBoardController.js
+++ controllers/dashBoardController.js
@@ -29,19 +29,13 @@ exports.getBillingRecordsByMonthAndYear = async (req, res) => {
     ],
   });
 
-  if (!billingRecords.length) {
-    return res
-      .status(404)
-      .json({ message: "Nenhum registro encontrado para este mês e ano." });
-  }
-
   const totalConsultations = billingRecords.reduce((acc, record) => {
     const daysArray = record.consultation_days
       ? record.consultation_days.split(",").map((day) => day.trim())
       : [];
     return acc + daysArray.length;
   }, 0);
-
+  
   const totalRevenue = billingRecords.reduce((acc, record) => {
     const daysArray = record.consultation_days
       ? record.consultation_days.split(",").map((day) => day.trim())
@@ -101,3 +95,32 @@ exports.getBillingRecordsByMonthAndYear = async (req, res) => {
     netTime,
   });
 };
+
+exports.Partialpayment = async (req,res) => {
+  const { customer_id, month_and_year, payment_amount} = req.body;
+
+  if (!customer_id || !month_and_year || payment_amount === undefined ){
+    return res.status(400).json({ error: "Dados incompletos." });
+  }
+
+  const billingRecord = await CustomersBillingRecords.findOne({
+    where: {customer_id, month_and_year},
+  });
+
+  if(!billingRecord){
+    return res.status(404).json({ error: "Registro não encontrado." });
+  }
+
+  await CustomersBillingRecords.update(
+    {
+      payment_amount: parseFloat(payment_amount).toFixed(2),
+      payment_status: "parcial",
+    },
+    {
+      where: { customer_id, month_and_year },
+    }
+  );
+
+  res.status(200).json({ message: "Pagamento parcial atualizado com sucesso." });
+
+}
--- routes/dashBoardRoutes.js
+++ routes/dashBoardRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const dashBoardController = require("../controllers/dashBoardController");
 
 router.get("/billing-records", dashBoardController.getBillingRecordsByMonthAndYear);
+router.post("/update-partial-payment", dashBoardController.Partialpayment);
 
 module.exports = router;
