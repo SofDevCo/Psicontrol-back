e5d20c60a20646dc9b2223151fe3a75381e824b3
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2025-02-11 19:03:31 -0300

    fix(api):"removes try-catchs"

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -11,101 +11,84 @@ const { Event, Calendar } = require("../models");
 
 const authenticateClient = async () => {
   if (!oauth2Client.credentials || !oauth2Client.credentials.access_token) {
-    try {
-      const tokens = await oauth2Client.refreshAccessToken();
-      oauth2Client.setCredentials(tokens.credentials);
-    } catch (error) {}
+    const tokens = await oauth2Client.refreshAccessToken();
+    oauth2Client.setCredentials(tokens.credentials);
   }
 };
 
 const createEventInGoogleCalendar = async (event, calendarId) => {
-  try {
-    const calendar = google.calendar({ version: "v3", auth: oauth2Client });
-    const response = await calendar.events.insert({
-      calendarId: calendarId,
-      resource: {
-        summary: event.event_name,
-        description: `Evento criado`,
-        start: {
-          dateTime: `${event.date}T${event.start_time}:00`,
-          timeZone: "America/Sao_Paulo",
-        },
-        end: {
-          dateTime: `${event.date}T${event.end_time}:00`,
-          timeZone: "America/Sao_Paulo",
-        },
+  const calendar = google.calendar({ version: "v3", auth: oauth2Client });
+  const response = await calendar.events.insert({
+    calendarId: calendarId,
+    resource: {
+      summary: event.event_name,
+      description: "Evento criado",
+      start: {
+        dateTime: `${event.date}T${event.start_time}:00`,
+        timeZone: "America/Sao_Paulo",
       },
-    });
-    return response.data.id;
-  } catch (error) {}
+      end: {
+        dateTime: `${event.date}T${event.end_time}:00`,
+        timeZone: "America/Sao_Paulo",
+      },
+    },
+  });
+  return response.data.id;
 };
 
 const deleteEventFromGoogleCalendar = async (calendarId, googleEventId) => {
-  try {
-    await authenticateClient();
-
-    const calendar = google.calendar({ version: "v3", auth: oauth2Client });
-
-    await calendar.events.delete({
-      calendarId: calendarId,
-      eventId: googleEventId,
-    });
-  } catch (error) {}
+  await authenticateClient();
+  const calendar = google.calendar({ version: "v3", auth: oauth2Client });
+  await calendar.events.delete({
+    calendarId: calendarId,
+    eventId: googleEventId,
+  });
 };
 
 exports.createEvent = async (req, res) => {
-  try {
-    const event = req.body;
-    if (
-      !event ||
-      !event.event_name ||
-      !event.date ||
-      !event.start_time ||
-      !event.end_time ||
-      !event.calendarId
-    ) {
-      return res.status(400).send("Dados inválidos.");
-    }
-
-    const googleEventId = await createEventInGoogleCalendar(
-      event,
-      event.calendarId
-    );
-
-    await saveEvent({
-      ...event,
-      google_event_id: googleEventId,
-    });
-
-    res.send("Evento criado com sucesso.");
-  } catch (error) {
-    res.status(500).send(error.message);
+  const event = req.body;
+  if (
+    !event ||
+    !event.event_name ||
+    !event.date ||
+    !event.start_time ||
+    !event.end_time ||
+    !event.calendarId
+  ) {
+    return res.status(400).send("Dados inválidos.");
   }
+
+  const googleEventId = await createEventInGoogleCalendar(
+    event,
+    event.calendarId
+  );
+
+  await saveEvent({
+    ...event,
+    google_event_id: googleEventId,
+  });
+
+  res.send("Evento criado com sucesso.");
 };
 
 exports.deleteEvent = async (req, res) => {
   const { google_event_id, calendarId } = req.params;
 
-  try {
-    if (!google_event_id || !calendarId) {
-      return res
-        .status(400)
-        .send("Os parâmetros google_event_id e calendarId são necessários.");
-    }
-
-    const existingEvent = await checkEventExists(google_event_id, calendarId);
-    if (!existingEvent) {
-      return res.status(404).send("Evento não encontrado no Google Calendar.");
-    }
-
-    await deleteEventFromGoogleCalendar(calendarId, google_event_id);
-
-    await cancelEventByGoogleId(google_event_id);
-
-    res.send("Evento cancelado com sucesso.");
-  } catch (error) {
-    res.status(500).send(`Erro interno do servidor: ${error.message}`);
+  if (!google_event_id || !calendarId) {
+    return res
+      .status(400)
+      .send("Os parâmetros google_event_id e calendarId são necessários.");
   }
+
+  const existingEvent = await checkEventExists(google_event_id, calendarId);
+  if (!existingEvent) {
+    return res.status(404).send("Evento não encontrado no Google Calendar.");
+  }
+
+  await deleteEventFromGoogleCalendar(calendarId, google_event_id);
+  await cancelEventByGoogleId(google_event_id);
+
+  res.send("Evento cancelado com sucesso.");
 };
 
 exports.deleteUnmatchedEvent = async (req, res) => {
@@ -136,36 +119,27 @@ exports.deleteUnmatchedEvent = async (req, res) => {
 };
 
 const checkEventExists = async (googleEventId, calendarId) => {
-  try {
-    await authenticateClient();
-    const calendar = google.calendar({ version: "v3", auth: oauth2Client });
-
-    const event = await calendar.events.get({
-      calendarId: calendarId,
-      eventId: googleEventId,
-    });
-
-    return event.data;
-  } catch (error) {}
+  await authenticateClient();
+  const calendar = google.calendar({ version: "v3", auth: oauth2Client });
+  const event = await calendar.events.get({
+    calendarId: calendarId,
+    eventId: googleEventId,
+  });
+  return event.data;
 };
 
 exports.syncCalendar = async (req, res) => {
   const { calendarId } = req.params;
-
-  try {
-    const tokens = oauth2Client.credentials;
-    if (!tokens || !tokens.access_token) {
-      return res
-        .status(401)
-        .send("Token de autenticação não encontrado. Faça login novamente.");
-    }
-
-    await syncGoogleCalendarWithDatabase(tokens.access_token, calendarId);
-
-    res.json({ message: "Calendário sincronizado com sucesso." });
-  } catch (error) {
-    res.status(500).send("Erro ao sincronizar o calendário.");
+  const tokens = oauth2Client.credentials;
+  if (!tokens || !tokens.access_token) {
+    return res
+      .status(401)
+      .send("Token de autenticação não encontrado. Faça login novamente.");
   }
+
+  await syncGoogleCalendarWithDatabase(tokens.access_token, calendarId);
+
+  res.json({ message: "Calendário sincronizado com sucesso." });
 };
 
 exports.listCalendars = async (req, res) => {
@@ -206,7 +180,7 @@ exports.getEventsByCalendar = async (req, res) => {
     where: {
       calendar_id: calendarId,
       user_id: req.user.user_id,
-      status: { [Op.notIn]: ["cancelado"] }
+      status: { [Op.notIn]: ["cancelado"] },
     },
   });
 
