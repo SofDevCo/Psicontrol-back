797b279ab80bc060abe3ee3fd954526aa10bff1f
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2025-03-11 09:04:00 -0300

    feat(api):"staging database"

--- config/config.js
+++ config/config.js
@@ -2,12 +2,19 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    use_env_variable: "DATABASE_URL"
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
   },
   test: {
-    use_env_variable: "DATABASE_URL"
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+  },
+  staging: {
+    use_env_variable: "DATABASE_URL_STAGING",
+    dialect: "postgres",
   },
   production: {
-    use_env_variable: "DATABASE_URL"
-  }
-}
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+  },
+};
