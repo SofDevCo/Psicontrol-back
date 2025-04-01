66634d652698512df58738bbf755c449cab1cb83
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2025-04-01 16:30:13 -0300

    fix(api):"format billing message"

---
+++ migrations/20250401192712-format-message.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.sequelize.query(`
+      ALTER TABLE "users"
+      ALTER COLUMN "user_message" SET DEFAULT 'Olá, {{nome}}!
+Estou enviando essa mensagem para passar os valores das consultas realizadas no mês de {{mês}}, nos dias {{dias}}.
+
+O valor total dos atendimentos é de R$ {{valor_total}}.
+
+Seguem abaixo os dados para pagamento:
+Código Pix:
+Banco: Agencia: 0000
+CC: 0000000
+
+Atenciosamente,
+Fulana(o) de tal';
+    `);
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.sequelize.query(`
+      ALTER TABLE "users"
+      ALTER COLUMN "user_message" DROP DEFAULT;
+    `);
+  },
+};
--- models/userModel.js
+++ models/userModel.js
@@ -49,8 +49,18 @@ const User = sequelize.define(
     user_message: {
       type: DataTypes.TEXT,
       allowNull: true,
-      defaultValue:
-        "Olá, {{nome}}! Estou enviando essa mensagem para passar os valores das consultas realizadas no mês de {{mês}}, nos dias {{dias}}. O valor total dos atendimentos é de R$ {{valor_total}}. Seguem abaixo os dados para pagamento: Código Pix: Banco: Agencia: 0000 CC: 0000000 Atenciosamente, Fulana(o) de tal"
+      defaultValue: `Olá, {{nome}}!
+      Estou enviando essa mensagem para passar os valores das consultas realizadas no mês de {{mês}}, nos dias {{dias}}.
+      
+      O valor total dos atendimentos é de R$ {{valor_total}}.
+      
+      Seguem abaixo os dados para pagamento:
+      Código Pix:
+      Banco: Agencia: 0000
+      CC: 0000000
+      
+      Atenciosamente,
+      Fulana(o) de tal`,
     },
     clinic_name: {
       type: DataTypes.STRING(255),
