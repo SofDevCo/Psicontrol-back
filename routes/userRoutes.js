2bc28e407d5ee9a6e5f86851067c237cad4b532e
OthavioFigueiredo <othaviosilvafigueiredo@gmail.com>
2025-04-29 11:05:23 -0300

    fix(api):"remove routes not used"

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.get("/users", userController.getUser);
-router.post("/register-user", userController.registerUser);
 router.put("/save-users", userController.editUser);
 
 module.exports = router;
